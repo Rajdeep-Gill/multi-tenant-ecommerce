2a9652dca729e4d3e666c820252a02b411d5bd63
Rajdeep Gill <35fear35@gmail.com>
2025-05-11 00:06:43 -0500

    Add nuqs and product filtering component

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -13,7 +13,17 @@ export const ProductList = ({ category }: Props) => {
     trpc.products.getMany.queryOptions({ category })
   );
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="grid gird-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {data?.docs.map((product) => (
+        <div key={product.id} className="border bg-white">
+          <h2 className="text-xl font-medium">{product.name}</h2>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export const ProductListSkeleton = () => {
